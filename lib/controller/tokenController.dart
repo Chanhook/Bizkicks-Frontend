57122ce3009bac406c5adefb0a0b1c9af8bff7e4
김찬혁 <cksgur97@gmail.com>
2021-09-22 20:37:39 +0900

    add tokencontroller

---
+++ lib/controller/tokenController.dart
@@ -0,0 +1,9 @@
+import 'package:get/state_manager.dart';
+
+class TokenController extends GetxController{
+  var grantType="";
+  var accessToken="";
+  var accessTokenExpiresIn=0;
+  var refreshToken="";
+
+}
